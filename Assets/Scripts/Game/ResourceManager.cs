a6cf16cfe04d079319932f86b62e385f835befbd
jikwang-park <pdk00137@gmail.com>
2025-02-16 17:26:18 +0900

    resourceManager 생성

---
+++ Assets/Scripts/Game/ResourceManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    enum ResourceType
+    {
+        Food,
+        ElectricalEnergy,
+        Oxygen,
+    }
+
+    public int food { get; private set; }
+
+    public int electricEnergy { get; private set; }
+    public int oxygen { get; private set; }
+
+    private ResourceType currentResourceType;
+
+
+    public void Init()
+    {
+
+    }
+
+    public void NextTurn()
+    {
+        food -= 1;
+        oxygen -= 2;
+        electricEnergy -= 1;
+    }
+
+    public void AddResource(ResourceType type,int value)
+    {
+        currentResourceType = type;
+        switch (currentResourceType)
+        {
+            case ResourceType.Food:
+
+                break;
+            case ResourceType.ElectricalEnergy:
+                break;
+            case ResourceType.Oxygen:
+                break;
+            default:
+                break;
+        }
+
+    }
+}
